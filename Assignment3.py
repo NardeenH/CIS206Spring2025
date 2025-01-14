16936f205cdc18bea23ced8cdd06f36ff19abed2
NardeenH <97866278+NardeenH@users.noreply.github.com>
2025-01-13 20:55:37 -0600

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,18 @@
+
+
+#3.	Create a program that shows the numerical values of the Boolean true and false. 
+
+
+true_value = True
+false_value = False
+
+print ("print numrical value of a is:")
+print(int(true_value))
+
+print ("print numrical value of b is:")
+print (int(false_value))
+
+
+
+
+

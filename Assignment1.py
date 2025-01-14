829b245fab9f275a2eda66d173456cb0af052290
NardeenH <97866278+NardeenH@users.noreply.github.com>
2025-01-13 20:52:50 -0600

    Delete Assignment1.py

--- Assignment1.py
+++
@@ -1,9 +0,0 @@
-
-#Problem #1 1.	Allow a user to enter first name and last name into two separate variables.
-# Then display the values in the order of last name, first name. 
-
-
-first_name = input(" Your First Name:")
-last_name = input(" Your Last Name:")
-print(first_name + " " + last_name)
-

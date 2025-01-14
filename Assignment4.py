c444c68dad80886bff7e8ca11ae81db9fc99eb01
NardeenH <97866278+NardeenH@users.noreply.github.com>
2025-01-13 20:48:23 -0600

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,12 @@
+
+
+#problem4:Allow a use to enter two floating point numerical values. Display the product of the two numbers. 
+
+
+num1= float(input("Enter  first floating number:"))
+num2 = float(input("Enter second floating number:")) 
+
+product = num1 * num2
+
+print(product)
+
